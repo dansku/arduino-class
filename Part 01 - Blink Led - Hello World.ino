25862e0afcd79f96a1ecd99d05d22a4320aa8cd6
Daniel Andrade <daniel@danielandrade.net>
2015-02-02 19:25:14 -0500

    Part 01

---
+++ Part 01 - Blink Led - Hello World.ino
@@ -0,0 +1,24 @@
+/*
+
+This is my first program, which is awesome!
+Name: Daniel Andrade
+
+*/
+
+int ledPin = 13;
+
+void setup(){
+
+	pinMode(ledPin,OUTPUT);
+  
+}
+
+void loop(){
+
+	digitalWrite(ledPin,HIGH):
+	delay(1000);
+  
+	digitalWrite(ledPin,HIGH):
+	delay(1000);
+  
+}
